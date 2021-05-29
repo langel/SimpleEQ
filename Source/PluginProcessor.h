cd8b37bd7cf3cc3f56eb9d92f025ce6dd5cee25a
langel <knoxburry@gmail.com>
2021-05-28 21:22:47 -0400

    refactored low cut filter process

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -20,7 +20,7 @@ enum Slope {
 struct ChainSettings {
 	float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
 	float lowCutFreq{ 0 }, highCutFreq{ 0 };
-	int lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
+	Slope lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
@@ -81,6 +81,39 @@ private:
 		LowCut,
 		Peak,
 		HighCut
+	};
+
+	void updatePeakFilter(const ChainSettings& chainSettings);
+	using Coefficients = Filter::CoefficientsPtr;
+	static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+	template<int Index, typename ChainType, typename CoefficientType>
+	void update(ChainType& chain, const CoefficientType& coefficients) {
+		updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+		chain.template setBypassed<Index>(false);
+	};
+
+	template<typename ChainType, typename CoefficientType>
+	void updateCutFilter(ChainType& chain, const CoefficientType& coefficients, const Slope& slope) {
+
+		chain.template setBypassed<0>(true);
+		chain.template setBypassed<1>(true);
+		chain.template setBypassed<2>(true);
+		chain.template setBypassed<3>(true);
+		switch (lowCutSlope) {
+			case Slope_48: {
+				update<3>(chain, coefficients);
+			}
+			case Slope_36: {
+				update<2>(chain, coefficients);
+			}
+			case Slope_24: {
+				update<1>(chain, coefficients);
+			}
+			case Slope_12: {
+				update<0>(chain, coefficients);
+			}
+		}
 	};
 
     //==============================================================================
