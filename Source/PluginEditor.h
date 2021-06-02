eb0117f054ed1371b64bfea6f2c84b3738d37c0c
langel <knoxburry@gmail.com>
2021-06-02 18:16:10 -0400

    refactored peak coefficient generation

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -21,7 +21,9 @@ struct CustomRotarySlider : juce::Slider {
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
+	juce::AudioProcessorParameter::Listener,
+	juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -31,10 +33,17 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+	void parameterValueChanged(int parameterIndex, float newValue) override;
+	void parameterGestureChanged(int paramaterIndex, bool gestureIsStarting) override { };
+
+	void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+	juce::Atomic<bool> parametersChanged{ false };
 
 	CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider;
 	CustomRotarySlider lowCutSlopeSlider, highCutSlopeSlider;
--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -208,16 +208,20 @@ ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts) {
 }
 
 
-void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings) {
-	auto peakCoefficients = juce::dsp::IIR::Coefficients<float>::makePeakFilter(getSampleRate(),
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate) {
+	return juce::dsp::IIR::Coefficients<float>::makePeakFilter(sampleRate,
 		chainSettings.peakFreq,
 		chainSettings.peakQuality,
 		juce::Decibels::decibelsToGain(chainSettings.peakGainInDecibels));
+}
+
+void SimpleEQAudioProcessor::updatePeakFilter(const ChainSettings& chainSettings) {
+	auto peakCoefficients = makePeakFilter(chainSettings, getSampleRate());
 	updateCoefficients(leftChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 	updateCoefficients(rightChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
 }
 
-void SimpleEQAudioProcessor::updateCoefficients(Coefficients& old, const Coefficients& replacements) {
+void updateCoefficients(Coefficients& old, const Coefficients& replacements) {
 	*old = *replacements;
 }
 
