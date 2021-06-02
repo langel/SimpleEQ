d53f071b6ab05b187c084fff7409fd3756f99f95
langel <knoxburry@gmail.com>
2021-06-01 21:45:35 -0400

    connected sliderswq

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -39,6 +39,12 @@ private:
 	CustomRotarySlider peakFreqSlider, peakGainSlider, peakQualitySlider, lowCutFreqSlider, highCutFreqSlider;
 	CustomRotarySlider lowCutSlopeSlider, highCutSlopeSlider;
 
+	using APVTS = juce::AudioProcessorValueTreeState;
+	using Attachment = APVTS::SliderAttachment;
+
+	Attachment peakFreqSliderAttachment, peakGainSliderAttachment, peakQualitySliderAttachment, lowCutFreqSliderAttachment, highCutFreqSliderAttachment;
+	Attachment lowCutSlopeSliderAttachment, highCutSlopeSliderAttachment;
+
 	std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
